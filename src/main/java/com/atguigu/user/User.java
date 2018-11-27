3932db0993a289d0a84b9ee13ddefc55f4d0ae24
yangshuai <454990668@qq.com>
2018-11-27 11:55:35 +0800

    delete all file

--- src/main/java/com/atguigu/user/User.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.user;
-
-public class User {
-	
-	public static void main(int args ,String argv[]) {
-		System.out.println("test");
-	}
-}
--- src/test/java/com/atguigu/test/UserTest.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.test;
-
-public class UserTest {
-
-	public void show() {
-		System.out.println("UserTest");
-	}
-}
